802aca3303a916a3b55a7cf2afbe3d35787c9d47
ankit kumar <ankitchotu2004bkj@gmail.com>
2025-06-24 05:23:41 +0000

    this is the path of the logo /home/user/studio/public/images/favicon.svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,9 +46,9 @@ export default function RootLayout({
       <head>
         <title>Ankit Kumar Portfolio</title>
         <meta name="description" content="Portfolio of Ankit Kumar, an AIML Enthusiast & Builder." />
-        <link rel="icon" href="/favicon.svg?v=2" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/favicon.svg?v=2" />
-        <link rel="shortcut icon" href="/favicon.svg?v=2" />
+        <link rel="icon" href="/images/favicon.svg?v=3" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/images/favicon.svg?v=3" />
+        <link rel="shortcut icon" href="/images/favicon.svg?v=3" />
         <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
         <meta name="theme-color" content="#222222" media="(prefers-color-scheme: dark)" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
